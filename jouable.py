fea90edb2ee981ae2671e09380de846c4fc2ca1b
Greg G. <greg.gionet@gmail.com>
2026-09-08 10:44:18 -0400

    Début du squelette
    début squelette et les virus

---
+++ jouable.py
@@ -0,0 +1,39 @@
+import random
+
+VIRUS = ["Bouncy Castle", "Invisible Men", "Like The Dinosaures", "Texture Not Found", "Upside Down All Around", "Black Hole", "Lag Spike"]
+ACTIVE_VIRUS = []
+
+def announceVirus():
+    print (ACTIVE_VIRUS[len(ACTIVE_VIRUS) - 1] + " is active!")
+
+def useVirus(firewall):
+    virus = []
+    for e in range(0, 2):
+        virus.append(random.randint(0,7))
+    print(firewall + " choisie un virus a injecter: ")
+    isInvalid = True
+    while isInvalid:
+        print("1 : " + VIRUS[virus[0]])
+        print("2 : " + VIRUS[virus[1]])
+        choix = input("Choix : ")
+        if(choix in "1", "2"):
+            isInvalid = False
+            ACTIVE_VIRUS.append(VIRUS[virus[int(choix)-1]])
+    announceVirus()
+
+
+def runGame(firewall, player1, player2, player3, gamemode, map) :    
+    useVirus(firewall)
+    
+
+def newGame() :
+    firewallPlayer = input("Qui est le Firewall?: ")
+    survivor1 = input("Qui est le premier survivant?: ")
+    survivor2 = input("Qui est le second survivant?: ")
+    survivor3 = input("Qui est le troisieme survivant?: ")
+    gamemode = input("Choisissez le gamemode: ")
+    map = input("Quelle est la carte de jeu?: ")
+    runGame(firewallPlayer, survivor1, survivor2, survivor3, gamemode, map) 
+
+if __name__ == "__main__":
+    newGame()
