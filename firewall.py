460945ac1608d134b7c1009e8eb41e7abf9fa9d5
ABernatchez <abernatchez@tutamail.com>
2026-09-08 10:15:34 -0400

    Added gamestates and choice for menu

--- firewall.py
+++ firewall.py
@@ -1,3 +1,11 @@
+from enum import Enum
+
+class GameState(Enum):
+    MENU_PRINCIPAL = 1
+    OPTION = 2
+    IN_GAME = 3
+
+
 def showMenu():
     print("FIREWALL: *courte description*")
     print("Membre de l'équipe:")
@@ -11,7 +19,36 @@ def showMenu():
     print("\n6 - Quitter")
 
 def run():
-    showMenu()
+    game_state = GameState.MENU_PRINCIPAL
+    running = True
+
+    while running:
+        match game_state:
+            case GameState.MENU_PRINCIPAL:
+                showMenu()
+                choice = input("\nEntrer votre choix: ")
+                match choice:
+                    case "1":
+                        pass
+                    case "2":
+                        pass
+                    case "3":
+                        pass
+                    case "4":
+                        pass
+                    case "5":
+                        game_state = GameState.OPTION
+                    case "6":
+                        running = False
+                    case _:
+                        print("Le choix n'est pas valide")
+            case GameState.OPTION:
+                print("Option: retour au menu principal")
+                game_state = GameState.MENU_PRINCIPAL
+
+    print("Exiting game...")
+
+    
 
 def load():
     pass
