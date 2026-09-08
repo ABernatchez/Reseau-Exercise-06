1d51e82d6acce6195bd73c57d21419d21f40b1ba
Greg G. <greg.gionet@gmail.com>
2026-09-08 10:49:55 -0400

    Gamemodes

--- jouable.py
+++ jouable.py
@@ -2,6 +2,7 @@ import random
 
 VIRUS = ["Bouncy Castle", "Invisible Men", "Like The Dinosaures", "Texture Not Found", "Upside Down All Around", "Black Hole", "Lag Spike"]
 ACTIVE_VIRUS = []
+GAMEMODES = ["Zombie", "King of The Hill", "Treasure Hunt", "Race to The Top"]
 
 def announceVirus():
     print (ACTIVE_VIRUS[len(ACTIVE_VIRUS) - 1] + " is active!")
@@ -22,18 +23,28 @@ def useVirus(firewall):
     announceVirus()
 
 
-def runGame(firewall, player1, player2, player3, gamemode, map) :    
+def runGame(player1, player2, player3, firewall) :  
+    gamemode = input("Choisissez le gamemode: ")  
+    isInvalid = True
+    while isInvalid:
+        print("1 : " + VIRUS[virus[0]])
+        print("2 : " + VIRUS[virus[1]])
+        choix = input("Choix : ")
+        if(choix in "1", "2"):
+            isInvalid = False
+            ACTIVE_VIRUS.append(VIRUS[virus[int(choix)-1]])
+    map = input("Quelle est la carte de jeu?: ")
+
     useVirus(firewall)
-    
+
+
 
 def newGame() :
     firewallPlayer = input("Qui est le Firewall?: ")
     survivor1 = input("Qui est le premier survivant?: ")
     survivor2 = input("Qui est le second survivant?: ")
     survivor3 = input("Qui est le troisieme survivant?: ")
-    gamemode = input("Choisissez le gamemode: ")
-    map = input("Quelle est la carte de jeu?: ")
-    runGame(firewallPlayer, survivor1, survivor2, survivor3, gamemode, map) 
+    runGame(firewallPlayer, survivor1, survivor2, survivor3) 
 
 if __name__ == "__main__":
     newGame()
