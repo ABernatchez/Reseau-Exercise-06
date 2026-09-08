27390b792e135d74efe2e0ccfab518e77516df3d
François-Xavier Thibault <fxthibault0@gmail.com>
2026-09-08 10:34:46 -0400

    Update gameState.py

--- gameState.py
+++ gameState.py
@@ -1,2 +1,19 @@
 import json
-Rounds = 0
+class Save ():
+    health = 100
+    dmg = 5
+    speed = 2
+    gameMaster = 3
+    def saveData(health, dmg, speed, gameMaster):
+        data = {
+            "Health": health,
+            "Damage": dmg,
+            "Speed": speed,
+            "Game Master": gameMaster
+        }
+        json_str = json.dumps(data, indent=4)
+        with open("Data.json", "w") as f:
+            f.write(json_str)
+
+
+#Source = https://www.geeksforgeeks.org/python/reading-and-writing-json-to-a-file-in-python/
