c03d272a1917db7aa4aeb04286df47e2c421f22d
Greg G. <greg.gionet@gmail.com>
2026-09-08 22:50:39 -0400

    Je me suis laissé aller...
    Ajout des maps, objets, scores, les joueurs peuvent mourir et il y a un simulateur d'évènement auquel on peut ajouter des idées.
    
    J'ai aussi clean up la selection de gamemodes et de maps.
    
    J'ai laisser l'affichage du score de fin en TODO.
    
    Si j'ai mal fais mon travaille dans la classe game_info, j'aimerais que quelqu'un vérifie ce que j'ai fait.

--- jouable.py
+++ jouable.py
@@ -6,15 +6,29 @@ VIRUS = [
     "Like The Dinosaures", 
     "Texture Not Found", 
     "Upside Down All Around", 
-    "Black Hole", "Lag Spike"
+    "Black Hole", 
+    "Lag Spike"
 ]
 GAMEMODES = ["Zombie", "King of The Hill", "Treasure Hunt", "Race to The Top"]
 
+MAP = ["Coconut Mall", "Mid-City Zoo", "Mega Arch Park"]
+
+OBJETS = ["Boom Box", "Office Chair", "Toy Gun", "Pogo Stick"]
+
+EFFET_OBJETS = [" a jouer une musique explosive!", " a roulé à toute allure loin de son bureau!", " a fais croire aux autres qu'Andy était là!", " a sauté au dessus de tous le chaos!"]
+
 class GameInfo():
     firewall_player = ""
+    firewall_score = 0
     survivor1 = ""
+    survivor_1_score = 0
+    survivor_1_dead = False
     survivor2 = ""
+    survivor_2_score = 0
+    survivor_2_dead = False
     survivor3 = ""
+    survivor_3_score = 0
+    survivor_3_dead = False
 
     gamemode = ""
     map = ""
@@ -24,26 +38,47 @@ class GameInfo():
     def __init__(
         self, 
         firewall = "", 
+        firewall_score=0,
         s1 = "", 
+        s1_score=0,
+        s1_dead=False,
         s2 = "", 
+        s2_score=0,
+        s2_dead=False,
         s3 = "",
+        s3_score=0,
+        s3_dead=False,
         gamemode = "",
         map = "",
         active_virus = []
     ):
         self.firewall_player = firewall
+        self.firewall_score = firewall_score
         self.survivor1 = s1,
+        self.s1_score=s1_score
+        self.s1_dead=s1_dead
         self.survivor2 = s2
+        self.s2_score=s2_score
+        self.s2_dead=s2_dead
         self.survivor3 = s3
+        self.s3_score=s3_score
+        self.s3_dead=s3_dead
         self.gamemode = gamemode
         self.map = map
         self.active_virus = active_virus
 
     def reset(self):
         self.firewall_player = ""
+        self.firewall_score = 0
         self.survivor1 = ""
+        self.s1_score=0
+        self.s1_dead=False
         self.survivor2 = ""
+        self.s2_score=0
+        self.s2_dead=False
         self.survivor3 = ""
+        self.s3_score=0
+        self.s3_dead=False
         self.gamemode = -1
         self.map = ""
         self.active_virus = []
@@ -56,14 +91,17 @@ class GameInfo():
         self.active_virus.append(VIRUS[index])
 
     def getGamemodeString(self):
-        return GAMEMODES[self.gamemode]
+        return GAMEMODES[int(self.gamemode)]
+
+    def getMapString(self):
+        return MAP[int(self.map)]
 
     def getLastVirus(self):
         return self.active_virus[len(self.active_virus) - 1]
 
     def getGameDisplay(self):
         str = f"\nFIREWALL\n----------\n"
-        str += f"{self.gamemode} sur la map {self.map}\n"
+        str += f"{self.getGamemodeString()} sur la map {self.getMapString}\n"
         str += f"Firewall: {self.firewall_player}\n"
         str += f"Survivants: {self.survivor1}, {self.survivor2}, {self.survivor3}\n"
         str += "----------\n"
@@ -109,24 +147,133 @@ def announceVirus():
 def useVirus(game_info):
     virus = []
     for e in range(0, 2):
-        virus.append(random.randint(0,7))
+        virus.append(random.randint(0,6))
     
-    print(game_info.firewallPlayer + ", choisie un virus a injecter: ")
+    print(game_info.firewall_player + ", choisie un virus a injecter: ")
     is_invalid = True
     while is_invalid:
         print("1 : " + VIRUS[virus[0]])
         print("2 : " + VIRUS[virus[1]])
         choix = input("Choix : ")
 
-        if(choix in "1", "2"):
+        if(choix in ("1", "2")):
             is_invalid = False
             game_info.addVirus(virus[int(choix)-1])
     
     announceVirus()
 
+def chooseGamemode():
+    for e in range (0, len(GAMEMODES)):
+        number = e + 1
+        print(str(number) + ": " + GAMEMODES[e])
+    is_invalid = True
+    while is_invalid:
+        choix = input("Choisissez le gamemode: ")
+        if(choix in ("1", "2", "3", "4")):
+            is_invalid = False
+            return choix
+        else:
+            print("Entrée invalide. Veuillez réessayer.") 
+
+def chooseMap():
+    for e in range (0, len(MAP)):
+        number = e + 1
+        print(str(number) + ": " + MAP[e])
+    is_invalid = True
+    while is_invalid:
+        choix = input("Choisissez la carte: ")
+        if(choix in ("1", "2", "3")):
+            is_invalid = False
+            return choix
+        else:
+            print("Entrée invalide. Veuillez réessayer.") 
+
+def roundStart():
+    print("1...")
+    print("2...")
+    print("3...")
+    print("GO!")
+
+def endRound(game_info, nbKills):
+    print("Round ended")
+    winner = random.randint(1, 4)
+    if(winner == 4):
+        game_info.firewall_score += 5
+    elif(winner == 3):
+        game_info.survivor_3_score += 5
+        game_info.firewall_score += nbKills
+    elif(winner == 2):
+        game_info.survivor_2_score += 5
+        game_info.firewall_score += nbKills
+    else:
+        game_info.survivor_1_score += 5
+        game_info.firewall_score += nbKills
+
+def killPlayer(game_info, player):
+    if(player==1):
+        game_info.survivor_1_dead=True
+        print(game_info.survivor1 + "est mort.")
+    elif(player==2):
+        game_info.survivor_2_dead=True
+        print(game_info.survivor2 + "est mort.")
+    elif(player==3):
+        game_info.survivor_3_dead=True
+        print(game_info.survivor3 + "est mort.")
+
+def spawnObjet(game_info):
+    objet = random.randint(0, 3)
+    print("L'objet \"" + OBJETS[objet] + "\" s'est glissé dans le code.")
+    onTheGround = True
+    while onTheGround:
+        pickupperNb = random.randint(1,3)
+        if(pickupperNb == 1 and not game_info.survivor_1_dead):
+            print(game_info.survivor1 + "a ramassé : " + OBJETS[objet] + ".")
+            print(game_info.survivor1 + EFFET_OBJETS[objet])
+        elif(pickupperNb == 2 and not game_info.survivor_2_dead):
+            print(game_info.survivor2 + "a ramassé : " + OBJETS[objet] + ".")
+            print(game_info.survivor2 + EFFET_OBJETS[objet])
+        elif(pickupperNb == 3 and not game_info.survivor_3_dead):
+            print(game_info.survivor3 + "a ramassé : " + OBJETS[objet] + ".")
+            print(game_info.survivor3 + EFFET_OBJETS[objet])
+        else:
+            print("Le code indésirable a été effacé.")
+
+def eventSimulation(game_info):
+    while True:
+        print("1: Survivant 1 meurt")
+        print("2: Survivant 2 meurt")
+        print("3: Survivant 3 meurt")
+        print("4: Un objet apparaît")
+        print("5: Mettre fin à la simulation")
+        choix = input("Que ce passe t'il?: ")
+        if(choix == "1" and not game_info.survivor_1_dead):
+            killPlayer(game_info, 1)
+        elif(choix == "2" and not game_info.survivor_2_dead):
+            killPlayer(game_info, 2)
+        elif(choix == "3" and not game_info.survivor_3_dead):
+            killPlayer(game_info, 3)
+        elif(choix == "4"):
+            spawnObjet(game_info)
+        elif(choix == "5"):
+            break
+        else:
+            print("Entrée invalide. Veuillez réessayer.") 
+        if(game_info.survivor_1_dead and game_info.survivor_2_dead and game_info.survivor_3_dead):
+            print("Tous les survivants sont morts. La manche est finie.")
+            break
+
+def endGame(game_info):
+    #TODO Afficher les scores et nommer le grand gagnant.
+    pass
+
 def runGame(game_info):
     print(game_info.getGameDisplay())
-    useVirus(game_info)
+    for e in range(0, 3):
+        roundStart()
+        useVirus(game_info)
+        eventSimulation(game_info)
+        endRound(game_info)
+    endGame(game_info)
 
 def newGame():
     game_info = GameInfo()
@@ -134,8 +281,8 @@ def newGame():
     game_info.survivor1 = input("Qui est le premier survivant?: ")
     game_info.survivor2 = input("Qui est le second survivant?: ")
     game_info.survivor3 = input("Qui est le troisieme survivant?: ")
-    game_info.gamemode = input("Choisissez le gamemode: ")
-    game_info.map = input("Quelle est la carte de jeu?: ")
+    game_info.gamemode = chooseGamemode()
+    game_info.map = chooseMap()
     return game_info
 
 
